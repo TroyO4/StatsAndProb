c1dd9a2c46ed358d7f30c231a10744e34d9d028a
troy <oconnort@go.stockton.edu>
2024-10-03 12:50:57 -0400

    update

--- montyhall/MontyHall.java
+++ montyhall/MontyHall.java
@@ -1,4 +1,58 @@
 package montyhall;
+import java.util.Random;
+
 public class MontyHall {
-    
+    public static void main(String[] args) {
+        int trials = 10000;
+        int stayWins = 0;
+        int switchWins = 0;
+        
+        stayWins = playGameNoSwitch(trials);
+        switchWins = playGameWithSwitch(trials);
+
+        System.out.println("Wins by staying: " + stayWins + " out of " + trials);
+        System.out.println("Wins by switching: " + switchWins + " out of " + trials);
+        System.out.println("Win percentage by staying: " + (stayWins * 100 / trials) + "%");
+        System.out.println("Win percentage by switching: " + (switchWins * 100 / trials) + "%");
+    }
+
+    public static int playGameNoSwitch(int trials) {
+        int wins = 0;
+        Random rand = new Random();
+
+        for (int i = 0; i < trials; i++) {
+            int goodPrize = rand.nextInt(3);
+            int contestantChoice = rand.nextInt(3);
+
+            if (contestantChoice == goodPrize) {
+                wins++;
+            }
+        }
+        return wins;
+    }
+
+    public static int playGameWithSwitch(int trials) {
+        int wins = 0;
+        Random rand = new Random();
+
+        for (int i = 0; i < trials; i++) {
+            int goodPrize = rand.nextInt(3);
+            int contestantChoice = rand.nextInt(3);
+
+            int hostReveal = rand.nextInt(3);
+            while (hostReveal == contestantChoice || hostReveal == goodPrize) {
+                hostReveal = rand.nextInt(3);
+            }
+
+            int switchChoice = rand.nextInt(3);
+            while (switchChoice == contestantChoice || switchChoice == hostReveal) {
+                switchChoice = rand.nextInt(3);
+            }
+
+            if (switchChoice == goodPrize) {
+                wins++;
+            }
+        }
+        return wins;
+    }
 }
