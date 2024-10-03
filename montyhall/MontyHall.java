059b5aea0c2123d5d9888a540ca1485afc0b9342
troy <oconnort@go.stockton.edu>
2024-10-03 13:35:15 -0400

    update to Main method

--- montyhall/MontyHall.java
+++ montyhall/MontyHall.java
@@ -7,58 +7,47 @@ package montyhall;
 import java.util.Random;
 
 public class MontyHall {
+    
     public static void main(String[] args) {
-        int trials = 10000;
-        int stayWins = 0;
-        int switchWins = 0;
-        
-        stayWins = playGameNoSwitch(trials);
-        switchWins = playGameWithSwitch(trials);
-
-        System.out.println("Wins by staying: " + stayWins + " out of " + trials);
-        System.out.println("Wins by switching: " + switchWins + " out of " + trials);
-        System.out.println("Win percentage by staying: " + (stayWins * 100 / trials) + "%");
-        System.out.println("Win percentage by switching: " + (switchWins * 100 / trials) + "%");
+        System.out.println("\nWins by staying: " + playGameNoSwitch(10000) + " out of 10,000");
+        System.out.println("Wins by switching: " + playGameWithSwitch(10000) + " out of 10,000");
+        System.out.println("Win percentage by staying: " + (playGameNoSwitch(10000) * 100 / 10000) + "%");
+        System.out.println("Win percentage by switching: " + (playGameWithSwitch(10000) * 100 / 10000) + "%\n");
     }
 
     public static int playGameNoSwitch(int trials) {
         int wins = 0;
         Random rand = new Random();
-
         for (int i = 0; i < trials; i++) {
-            int goodPrize = rand.nextInt(3);
-            int contestantChoice = rand.nextInt(3);
-
+            int goodPrize = rand.nextInt(3); // Random door for good prize
+            int contestantChoice = rand.nextInt(3); // Contestant picks random door
             if (contestantChoice == goodPrize) {
-                wins++;
+                wins++; // Contestant wins if they pick the door with the prize
             }
         }
-        return wins;
+        return wins; // Return total number of wins by staying
     }
 
     public static int playGameWithSwitch(int trials) {
         int wins = 0;
         Random rand = new Random();
-
         for (int i = 0; i < trials; i++) {
-            int goodPrize = rand.nextInt(3);
-            int contestantChoice = rand.nextInt(3);
+            int goodPrize = rand.nextInt(3); // Random door for good prize
+            int contestantChoice = rand.nextInt(3); // Contestant picks random door
 
+            // Host opens a dud door
             int hostReveal = rand.nextInt(3);
             while (hostReveal == contestantChoice || hostReveal == goodPrize) {
-                hostReveal = rand.nextInt(3);
+                hostReveal = rand.nextInt(3); // Host keeps revealing until dud is shown
             }
 
-            int switchChoice = rand.nextInt(3);
-            while (switchChoice == contestantChoice || switchChoice == hostReveal) {
-                switchChoice = rand.nextInt(3);
-            }
-
+            // Contestant switches to remaining unopened door
+            int switchChoice = 3 - contestantChoice - hostReveal; // Simple switch logic
             if (switchChoice == goodPrize) {
-                wins++;
+                wins++; // Contestant wins by switching
             }
         }
-        return wins;
+        return wins; // Return total number of wins by switching
     }
 }
 
