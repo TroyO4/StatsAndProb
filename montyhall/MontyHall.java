3da554ef84980cb5bcb5f3b823416f1790715cfd
troy <oconnort@go.stockton.edu>
2024-10-03 13:17:31 -0400

    ,

--- montyhall/MontyHall.java
+++ montyhall/MontyHall.java
@@ -1,6 +1,7 @@
 /*
  * Troy O'Connor
  * Probability and Applied Stats
+ * Answers to A and B at bottom of code
  */
 package montyhall;
 import java.util.Random;
@@ -82,28 +83,28 @@ public class MontyHall {
  * you will discover that, perhaps surprisingly, this question can be answered by considering only 
  * the sample space above and using the probabilities that you assigned to answer part (a).
 
-i If the contestant choses to stay with her initial choice, she wins the good prize if and only if 
+i. If the contestant choses to stay with her initial choice, she wins the good prize if and only if 
 she initially chose curtain G. If she stays with her initial choice, what is the probability that 
 she wins the good prize?
 
     Answer: if the contestant stays with their intial choice they would have a 1/3 change of winning.
 
-ii If the host shows her one of the duds and she switches to the other unopened curtain, what will 
+ii. If the host shows her one of the duds and she switches to the other unopened curtain, what will 
 be the result if she had initially selected G?
 
     Answer: If she had originally selected G and then she switches then she will have a dud and lose
 
-iii Answer the question in part (ii) if she had initially selected one of the duds.
+iii. Answer the question in part (ii) if she had initially selected one of the duds.
 
     Answer: If she had originally selected a dud and then switches then she will have switched to the prize.
 
-iv If the contestant switches from her initial choice (as the result of being shown one of
+iv. If the contestant switches from her initial choice (as the result of being shown one of
 the duds), what is the probability that the contestant wins the good prize?
 
     Answer: The contestant had a 2/3 chance of choosing one of the duds right off the bat, and if she did originally 
     pull the dud than she switches, she would win. So her probability of winning by switching is 2/3
 
-v Which strategy maximizes the contestant’s probability of winning the good prize: stay
+v. Which strategy maximizes the contestant’s probability of winning the good prize: stay
 with the initial choice or switch to the other curtain?
 
     Answer: The probability of winning if the contestant decides to stay with the first choice is 1/3,
